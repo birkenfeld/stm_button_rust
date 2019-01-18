3bb2bb219f3090b97c74fa5e489fe17b24b84866
Georg Brandl <g.brandl@fz-juelich.de>
2019-01-18 11:28:24 +0100

    dma audio from sdcard

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 #![no_main]
 #![no_std]
-#![feature(try_blocks)]
 
-use panic_semihosting;
+use core::mem::swap;
 use stm32l0::stm32l0x3 as stm;
 // use stm32l0::stm32l0x3::interrupt;
 use cortex_m_rt::ExceptionFrame;
@@ -77,6 +76,9 @@ impl sd::TimeSource for Clock {
     }
 }
 
+static mut AUDIO1: [u8; 1024] = [0; 1024];
+static mut AUDIO2: [u8; 1024] = [0; 1024];
+
 #[cortex_m_rt::entry]
 fn main() -> ! {
     let mut pcore = arm::Peripherals::take().unwrap();
@@ -106,25 +108,79 @@ fn main() -> ! {
     modif!(GPIOB.ospeedr: ospeed3 = 0b10, ospeed5 = 0b10);
     modif!(GPIOB.afrl: afsel3 = 0, afsel4 = 0, afsel5 = 0);
     modif!(RCC.apb2enr: spi1en = true);
-    write!(SPI1.cr1: mstr = true, br = 0b101, ssm = true, ssi = true, spe = true);
+    write!(SPI1.cr1: mstr = true, br = 0b101,
+           ssi = true, ssm = true, spe = true);
 
+    // Set up DMA, DAC and timer for audio output
+    modif!(RCC.ahbenr: dmaen = true);
+    modif!(RCC.apb1enr: dacen = true, tim6en = true);
+
+    write!(DAC.cr: tsel1 = 0b000,  // trigger by TIM6 TRGO
+           dmaen1 = true,
+           en1 = true);
+    write!(DAC.dhr8r1: dacc1dhr = 0);
+
+    write!(DMA1.cpar2: pa = 0x4000_7410);  // DHR 12bit
+    write!(DMA1.cselr: c2s = 0b1001);
+    write!(DMA1.ccr2: circ = true, dir = true, minc = true, pl = 0b11, // very high
+           msize = 0, psize = 0);
+    write!(DMA1.cndtr2: ndt = 1024);
+
+    write!(TIM6.arr: arr = 191*8);
+    write!(TIM6.egr: ug = true);
+    write!(TIM6.cr2: mms = 0b010); // trigger on update
+    write!(TIM6.dier: ude = true);
+
+    // Initialize SD card
     let mut cont = sd::Controller::new(sd::SdMmcSpi::new(SPI, CS), Clock);
-    sh::hprintln!("Init SD card...").unwrap();
     cont.device().init().unwrap();
+    // Speed up SPI to maximum (8 MHz)
     modif!(SPI1.cr1: br = 0b000);
-    let res: Result<_, sd::Error<sd::SdMmcError>> = try {
-        let vol = cont.get_volume(sd::VolumeIdx(0))?;
-        let root = cont.open_root_dir(&vol)?;
-        let mut fd = cont.open_file_in_dir(&vol, &root, "audio.raw", sd::Mode::ReadOnly)?;
-        let mut buf = [0u8; 1024];
-        while cont.read(&vol, &mut fd, &mut buf)? == 1024 {}
-    };
-    if let Err(e) = res {
-        sh::hprintln!("Err: {:?}", e).unwrap();
-    } else {
-        sh::hprintln!("Reading done.").unwrap();
-    }
+    let vol = cont.get_volume(sd::VolumeIdx(0)).unwrap();
+    let root = cont.open_root_dir(&vol).unwrap();
+    let mut fd = cont.open_file_in_dir(&vol, &root, "audio.raw", sd::Mode::ReadOnly).unwrap();
 
+    'outer: loop {
+        // Clear output
+        write!(DAC.dhr8r1: dacc1dhr = 0);
+
+        // Set up buffer pointers
+        let (mut ptr1, mut ptr2) = unsafe { (&mut AUDIO1, &mut AUDIO2) };
+
+        // Read first block into first buffer
+        fd.seek_from_start(0).unwrap();
+        cont.read(&vol, &mut fd, ptr1).unwrap();
+        write!(DMA1.cmar2: ma = ptr1.as_ptr() as u32);
+
+        // Wait for button press
+        while readb!(GPIOC.idr: id13) {}
+
+        write!(TIM6.cr1: cen = true);
+        modif!(DMA1.ccr2: en = true);
+
+        loop {
+            // Clear DMA transfer flags
+            write!(DMA1.ifcr: ctcif2 = true, chtif2 = true, cgif2 = true, cteif2 = true);
+
+            // Read next block into buffer
+            if cont.read(&vol, &mut fd, ptr2).unwrap() == 0 {
+                // On end of file, deactivate DMA
+                write!(TIM6.cr1: cen = false);
+                modif!(DMA1.ccr2: en = false);
+                break;
+            }
+            write!(DMA1.cmar2: ma = ptr2.as_ptr() as u32);
+            swap(&mut ptr1, &mut ptr2);
+
+            // Wait for half-done flag
+            while !readb!(DMA1.isr: htif2) {}
+        }
+    }
+}
+
+#[panic_handler]
+fn panicking(_: &core::panic::PanicInfo) -> ! {
+    let _ = sh::hprintln!("panic!");
     loop {}
 }
 
